f58a5021c2d512a278c06f9f4680edfa74f84d3b
Ankur Mittal <anmittal@microsoft.com>
2023-04-21 13:47:58 +0530

    Removed K8 - will be checkin in couple of days

--- k8s/build-docker-images.ps1
+++
@@ -1,6 +0,0 @@
-$path = $MyInvocation.MyCommand.Path | Split-Path
-
-& docker build --tag dapr-asha/mosquitto:1.0 "$path/mosquitto"
-& docker build --tag dapr-asha/ashaservice:1.0 "$path/../ashaservice"
-& docker build --tag dapr-asha/anmservice:1.0 "$path/../anmservice"
-& docker build --tag dapr-asha/simulation:1.0 "$path/../simulation"
